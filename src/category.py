5ad6857aa2ccc4eebca25002a8ca5adeddb59fcb
Kseniya Kabanova <Ksusha1234567810@gmail.com>
2024-07-31 18:47:32 +0430

    add metods in category

--- src/category.py
+++ src/category.py
@@ -14,6 +14,22 @@ class Category:
         """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.product_count += len(products) if products else 0
         Category.category_count += 1
+
+    def add_product(self, product: Product):
+        """Метод для добавления товаров в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        products = ""
+        for product in self.__products:
+            products += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products
+
+    @property
+    def products_in_list(self):
+        return self.__products
